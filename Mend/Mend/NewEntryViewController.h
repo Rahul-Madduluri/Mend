f6af9492f7dc1a61d53d6c63947e346ea5fcc8be
rahulmadduluri <maddulur@usc.edu>
2015-01-25 00:58:51 -0800

    added new entry view controller

---
+++ Mend/Mend/NewEntryViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewEntryViewController.h
+//  Mend
+//
+//  Created by Rahul Madduluri on 1/25/15.
+//  Copyright (c) 2015 mend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewEntryViewController : UIViewController
+
+@end
